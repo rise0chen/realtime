c7b512cd8005a60f3aa38b497d7e664aa96c515d
rise0chen <rise0chen@163.com>
2022-07-14 19:19:12 +0800

    fix: saft link

--- rt_rtai/build.rs
+++ rt_rtai/build.rs
@@ -1,4 +1,12 @@
+use std::fs;
+use std::path::Path;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let asm_path = Path::new("vendor/rtai/include/asm");
+    let asm_origin = "./asm-x86";
+    let _ = fs::remove_file(&asm_path);
+    std::os::unix::fs::symlink(asm_origin, asm_path).unwrap();
+
     println!("cargo:rerun-if-changed=vendor/rtai/rtai.c");
     cc::Build::new()
         .file("vendor/rtai/rtai.c")
